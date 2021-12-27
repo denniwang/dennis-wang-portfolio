d6c60772236bf7779daac61c6f017ef7e281bd90
hellolol2016 <dennisthederp812@gmail.com>
2021-12-26 16:39:02 -0800

    did i mention im cracked

--- components/animatedSection.js
+++ components/animatedSection.js
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { motion } from "framer-motion";
+import { Center, VStack } from "@chakra-ui/react";
 let easing = [0.6, -0.05, 0.01, 0.99];
 
 export default function AnimatedSection({ title,children, ...rest}) {
@@ -11,9 +12,13 @@ export default function AnimatedSection({ title,children, ...rest}) {
         animate={{ opacity: 1 , y:0, transition:{duration:0.6}}}
       initial={{ opacity: 0, y: 30 }}
       {...rest}
-        className='title'>
-       {children} 
-        
+      className='title'>
+       
+      <Center>
+        <VStack>
+          {children} 
+          </VStack>
+        </Center>
         </motion.div>
         
   
--- pages/index.js
+++ pages/index.js
@@ -9,21 +9,43 @@ export default function Home() {
       style={{ backgroundColor: "transparent", color: "#e8eddf" }}
     >
       <Center bg={"#242423"} borderRadius="md" m={3}>
-        <Box color={"#e8eddf"} fontSize={"17px"} padding={"3"} >
-          Hello, I am a high school student in Southern California, welcome to my page!
+        <Box color={"#e8eddf"} fontSize={"17px"} padding={"3"}>
+          Hello, I am a high school student in Southern California, welcome to
+          my page!
         </Box>
       </Center>
-      <Center>
-        <Image src="me.png" maxWidth={"200px"} borderRadius={"full"} border={"3px solid #242423"}/>
 
-        <VStack justify={"flex-start"}>
-          <Box ml="5">
+      <Center margin={"0 10px"} flexDirection={{ base:"column", md:"row"}}>
+        <Image
+          src="me.png"
+          maxWidth={"200px"}
+          borderRadius={"full"}
+          border={"3px solid #242423"}
+        />
+
+        <VStack justify={"flex-start"} >
+          <Box ml="5" width={{base:"100%",xl:"auto"}}>
             <Text fontSize={"24px"} fontWeight={"bold"}>
               Dennis Wang
             </Text>
             <Text fontSize={"20px"}>Likes to learn new things &#128526;</Text>
             <Text>(Currently Web Dev)</Text>
           </Box>
+        </VStack>
+      </Center>
+
+      <Center width={"60%"}>
+        <VStack align={"end"} justify={"center"}>
+          <Text textDecoration={"underline 4px"} className="miniheader">
+            About
+          </Text>
+          <Text fontSize={"18px"} textAlign={"right"}>
+            Dennis is a high school student in Southern California, US. He has a
+            passion for anything related to web development, tennis, and
+            finances. In real life, Dennis can be found studying, working for
+            his nonprofit organization, or playing tennis. He currently does not
+            have a job but wouldn't mind getting one for some startup company ;)
+          </Text>
         </VStack>
       </Center>
     </AnimatedSection>
